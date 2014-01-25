206b5424641ba5d245879c8c6c764da43053d8fb
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-01-25 19:29:20 +0000

    setup: Cleanup.

--- setup.py
+++ setup.py
@@ -1,32 +1,37 @@
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+VERSION = open('VERSION').read().strip()
+
+REQUIRES = open('requirements.txt').read().strip().split('\n')
+
 
 setup(
-  name = 'jinjatools',
-  version = '0.1a12',
-  description = (
+  name='jinjatools',
+  version =VERSION,
+  description=(
     'Various tools for Jinja2,'
     ' including new filters and tests based on moretools,'
     ' a JinjaLoader class for Django,'
     ' and a simple JinjaBuilder class for SCons.'
     ),
-  author = 'Stefan Zimmermann',
-  author_email = 'zimmermann.code@gmail.com',
-  url = 'http://bitbucket.org/userzimmermann/python-jinjatools',
+  author='Stefan Zimmermann',
+  author_email='zimmermann.code@gmail.com',
+  url='http://bitbucket.org/userzimmermann/python-jinjatools',
 
-  license = 'LGPLv3',
+  license='LGPLv3',
 
-  install_requires = [
-    'six',
-    'moretools >= 0.1a25',
-    'Jinja2',
-    ],
-  packages = [
+  install_requires=REQUIRES,
+  packages=[
     'jinjatools',
     'jinjatools.django',
     'jinjatools.scons',
     ],
 
-  classifiers = [
+  classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'License :: OSI Approved ::'
@@ -38,7 +43,7 @@ setup(
     'Topic :: Software Development',
     'Topic :: Utilities',
     ],
-  keywords = [
+  keywords=[
     'jinja', 'jinja2', 'tools', 'template', 'macro',
     'filter', 'test', 'moretools',
     'scons', 'builder', 'build',
