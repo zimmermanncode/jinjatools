4948a49c4f562fe3158ca2c0377e7d554a50d3aa
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-07-14 15:10:54 +0000

    zetup

--- setup.py
+++ setup.py
@@ -1,53 +1,18 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# Run the zetup script:
+exec(open('__init__.py').read())
 
 
-VERSION = open('VERSION').read().strip()
-
-REQUIRES = open('requirements.txt').read().strip().split('\n')
-
-
-setup(
-  name='jinjatools',
-  version =VERSION,
-  description=(
-    'Various tools for Jinja2,'
-    ' including new filters and tests based on moretools,'
-    ' a JinjaLoader class for Django,'
-    ' and a simple JinjaBuilder class for SCons.'
-    ),
-  author='Stefan Zimmermann',
-  author_email='zimmermann.code@gmail.com',
-  url='http://bitbucket.org/userzimmermann/python-jinjatools',
-
-  license='LGPLv3',
-
-  install_requires=REQUIRES,
+zetup(
+  package_dir={
+    'jinjatools.zetup': '.',
+    },
   packages=[
     'jinjatools',
+    'jinjatools.zetup',
     'jinjatools.django',
     'jinjatools.scons',
     ],
-
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved ::'
-    ' GNU Library or Lesser General Public License (LGPL)',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 3',
-    'Topic :: Software Development',
-    'Topic :: Utilities',
-    ],
-  keywords=[
-    'jinja', 'jinja2', 'tools', 'template', 'macro',
-    'filter', 'test', 'moretools',
-    'scons', 'builder', 'build',
-    'django', 'web', 'html',
-    'python3',
-    ],
+  package_data={
+    'jinjatools.zetup': ZETUP_DATA,
+    },
   )
