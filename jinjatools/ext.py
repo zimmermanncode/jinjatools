956ed384e20347a0137ad2c3300b9e078c7f7d78
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-02-21 18:54:05 +0000

    updated project name and copyright years in file headers

--- jinjatools/ext.py
+++ jinjatools/ext.py
@@ -1,24 +1,24 @@
-# jinja-tools
+# python-jinjatools
 #
 # Various tools for Jinja2,
 # including new filters and tests based on python-moretools,
 # a JinjaLoader class for Django,
 # and a simple JinjaBuilder class for SCons.
 #
-# Copyright (C) 2011-2013 Stefan Zimmermann <zimmermann.code@gmail.com>
+# Copyright (C) 2011-2015 Stefan Zimmermann <zimmermann.code@gmail.com>
 #
-# jinja-tools is free software: you can redistribute it and/or modify
+# python-jinjatools is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# jinja-tools is distributed in the hope that it will be useful,
+# python-jinjatools is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with jinja-tools.  If not, see <http://www.gnu.org/licenses/>.
+# along with python-jinjatools.  If not, see <http://www.gnu.org/licenses/>.
 
 from six import add_metaclass
 
